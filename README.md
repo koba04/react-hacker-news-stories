aac95f00e0745a5238a55be6bd9c96264a5dd299
Toru Kobayashi <koba0004@gmail.com>
2015-04-20 20:55:07 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# React HackerNews Stories
+
+React sample app using HackerNews API for #html5j.
+
+```
+npm install
+npm run
+```
---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import {} from 'whatwg-fetch';
+import App from './lib/App';
+
+React.render(<App count={100} />, document.getElementById('app'));
