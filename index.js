f2a3a06288986a8c626118011a1f3b68cbd3b477
Toru Kobayashi <koba0004@gmail.com>
2015-04-22 01:22:00 +0900

    Flux implementation

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import {} from 'whatwg-fetch';
-import App from './lib/App';
+import App from './lib/components/App';
 
 React.render(<App count={100} />, document.getElementById('app'));
