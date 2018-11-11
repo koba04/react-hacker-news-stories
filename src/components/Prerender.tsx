eef2074d45cf4b1ae885fe56c82b9b098b69bdd3
Toru Kobayashi <koba0004@gmail.com>
2018-11-11 23:16:39 +0900

    Add Prerender component

---
+++ src/components/Prerender.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+interface Props {
+  children: React.ReactNode;
+  visible: boolean;
+}
+const Prerender = ({ children, visible }: Props) => (
+  <div hidden={!visible}>{children}</div>
+);
+export default Prerender;
