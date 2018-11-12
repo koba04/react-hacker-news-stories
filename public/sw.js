cd5f77f6b24b41f015785bfe6b7d202cdea5886b
Toru Kobayashi <koba0004@gmail.com>
2018-11-13 00:52:02 +0900

    Fix to cache in ServiceWorker

--- public/sw.js
+++ public/sw.js
@@ -1,22 +1,25 @@
 const CACHE_NAME = "hacker_news_api";
+const API_DELAY = 2000;
+
+const wait = ms => new Promise(r => setTimeout(r, ms));
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    (async () => {
       const isCacheTarget =
         event.request.url.indexOf("hacker-news.firebaseio.com") !== -1;
-      if (response & isCacheTarget) {
+      if (!isCacheTarget) {
+        return fetch(event.request);
+      }
+      const response = await caches.match(event.request);
+      if (response) {
+        await wait(API_DELAY);
         return response;
       }
-      return fetch(event.request).then(res => {
-        if (isCacheTarget) {
-          return caches.open(CACHE_NAME).then(cache => {
-            cache.put(event.request.url, res.clone());
-            return res;
-          });
-        }
-        return res;
-      });
-    })
+      const res = await fetch(event.request);
+      const cache = await caches.open(CACHE_NAME);
+      await cache.put(event.request.url, res.clone());
+      return res;
+    })()
   );
 });
